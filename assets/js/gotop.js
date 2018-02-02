ddb65601119971616c90be93e4850c47fb834e0d
joytou <1540294142@qq.com>
2018-02-02 16:25:24 +0800

    修复'返回顶部'无响应的错误

--- assets/js/gotop.js
+++ assets/js/gotop.js
@@ -1,17 +1,11 @@
-BackTop=function(btnId,DynamicDisplay){
-	var btn = document.getElementById(btnId);
-	var d = document.documentElement;
-	window.onscroll = set;
-	btn.onclick = function(){
-		btn.style.display = "none";
-		window.onscroll = null;
-		this.timer = setInterval(function(){
-			d.scrollTop-= Math.ceil(d.scrollTop * 0.1);
-			if(d.scrollTop == 0)clearInterval(btn.timer,window.onscroll=set);
-		},10);
+$(document).ready(function(){
+	function pageScroll() {
+		window.scrollBy(0,-10);
+		scrolldelay=setTimeout('pageScroll()',100);
 	};
-	function set(){
-		if(DynamicDisplay)btn.style.display = d.scrollTop?'block':"none";
-	};
-};
-BackTop('gotop',true);
+	$("#gotop").click(function(){
+		pageScroll();
+	});
+	if(document.documentElement.scrollTop==0)clearTimeout(scrolldelay);
+});
+
