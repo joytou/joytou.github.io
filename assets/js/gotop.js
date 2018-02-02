c1677c1a925910cd79c0e272a09ba6b070336a45
joytou <1540294142@qq.com>
2018-02-02 15:56:59 +0800

    '返回顶部'更改为悬浮按钮

---
+++ assets/js/gotop.js
@@ -0,0 +1,17 @@
+BackTop=function(btnId,DynamicDisplay){
+	var btn = document.getElementById(btnId);
+	var d = document.documentElement;
+	window.onscroll = set;
+	btn.onclick = function(){
+		btn.style.display = "none";
+		window.onscroll = null;
+		this.timer = setInterval(function(){
+			d.scrollTop-= Math.ceil(d.scrollTop * 0.1);
+			if(d.scrollTop == 0)clearInterval(btn.timer,window.onscroll=set);
+		},10);
+	};
+	function set(){
+		if(DynamicDisplay)btn.style.display = d.scrollTop?'block':"none";
+	};
+};
+BackTop('gotop',false);
