de12ccd821739ec96e8ddd38019e71964c842b74
joytou <1540294142@qq.com>
2018-02-27 17:15:35 +0800

    修复八哥

--- assets/js/languages.js
+++ assets/js/languages.js
@@ -2,4 +2,4 @@ var language = navigator.browserLanguage?navigator.browserLanguage:navigator.lan
 if (language.indexOf('en') > -1) window.location.replace('/en/'); 
 if (language.indexOf('CN') > -1) window.location.replace('/cn/'); 
 if (language.indexOf('HK') > -1||language.indexOf('MO') > -1||language.indexOf('TW') > -1) window.location.replace('/tw/'); 
-return false;
+//return false;
